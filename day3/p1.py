92f725d8f5bd7d9e6336099381ccdac113b54618
Adi Parmar <adiadi@google.com>
2021-12-03 00:21:58 -0500

    day3 p1

---
+++ day3/p1.py
@@ -0,0 +1,24 @@
+with open('inputs/p1/input.txt') as input:
+  inputList = list(input.read().split("\n"))
+  input.close()
+
+size = 12
+
+gammaBinaryCounts = [0] * 12
+
+for i in range(12):
+    for num in inputList:
+      if num[i] == "1":
+        gammaBinaryCounts[i]+=1
+
+gamma = 0
+epsilon = 0
+
+for i in range(12):
+  if gammaBinaryCounts[11-i] > 500:
+    gamma+= 2**i
+  else:
+    epsilon += 2**i
+
+print(gamma * epsilon)
+
