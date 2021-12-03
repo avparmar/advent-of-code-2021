54cc982d661ac50ab7625daf2784d379c9bbda45
Adi Parmar <adiadi@google.com>
2021-12-02 19:42:43 -0500

    adding day1 code

---
+++ day1.py
@@ -0,0 +1,15 @@
+with open('inputs/day1/input.txt') as input:
+  inputList = input.read().split("\n")
+  input.close()
+  
+increases = 0
+
+for i, measurement in enumerate(inputList):
+  if i == 0:
+    continue
+  if inputList[i] > inputList[i-1]:
+    increases+=1
+
+print(increases)
+    
+  
