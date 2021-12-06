7ecdb7f7349b78e8b5a9d909edd47fe9259d0303
Adi Parmar <adiadi@google.com>
2021-12-06 00:45:20 -0500

    day6 p1 and p2

---
+++ day6/p2.py
@@ -0,0 +1,17 @@
+with open('inputs/p1/input.txt') as input:
+  inputList = list(map(int, (input.readline().split(","))))
+  input.close()
+
+fish = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for entry in inputList:
+  fish[entry] += 1
+
+for i in range(256):
+  newFish = fish[0]
+  for j in range(8):
+    fish[j] = fish[j+1]
+  fish[6] += newFish
+  fish[8] = newFish
+
+print(sum(fish))
